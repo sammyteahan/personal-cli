5707dccf4409714bb4198727bff5c74c44d217b7
sammyteahan <sammyteahan@gmail.com>
2020-12-04 14:13:17 -0700

    Add gitlab link

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ welcome({
 
 console.log(`${chalk.italic('Software Developer at https://www.objectiveinc.com')}
 
-${chalk.hex('#6cc644').bold.inverse(' Github ')}         ${chalk.dim('https://github.com/sammyteahan')}
+${chalk.hex('#6cc644').bold.inverse(' GitHub ')}         ${chalk.dim('https://github.com/sammyteahan')}
+${chalk.hex('#554488').bold.inverse(' GitLab ')}         ${chalk.dim('https://gitlab.com/sammyteahan')}
 ${chalk.hex('#f48024').bold.inverse(' Stack Overflow ')} ${chalk.dim('https://stackoverflow.com/users/7003729/sammyteahan')}
 ${chalk.hex('#C11D26').bold.inverse(' Codeforces ')}     ${chalk.dim('https://codeforces.com/submissions/kingjames')}
 `);
