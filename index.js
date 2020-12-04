245778f5459a9a45973578c41af7db385f143f16
sammyteahan <sammyteahan@gmail.com>
2020-12-04 12:52:29 -0700

    Firsties

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+console.log(`
+Sammy Teahan
+
+Software Developer at Objective
+
+Github: https://github.com/sammyteahan
+Stack Overflow: https://stackoverflow.com/users/7003729/sammyteahan
+Codeforces: https://codeforces.com/submissions/kingjames
+`);
+
