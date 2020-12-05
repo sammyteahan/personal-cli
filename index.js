be193226740210570da53a89a193988718895732
sammyteahan <sammyteahan@gmail.com>
2020-12-05 13:16:39 -0700

    Refactor IIFE

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const chalk = require('chalk');
 
 const init = require('./src/init');
 
-init();
+(() => {
+  init();
 
-(function() {
   console.log(`${chalk.italic('Software Developer at https://www.objectiveinc.com')}
 
   ${chalk.hex('#6cc644').bold.inverse(' GitHub ')}         ${chalk.dim('https://github.com/sammyteahan')}
@@ -14,5 +14,5 @@ init();
   ${chalk.hex('#f48024').bold.inverse(' Stack Overflow ')} ${chalk.dim('https://stackoverflow.com/users/7003729/sammyteahan')}
   ${chalk.hex('#C11D26').bold.inverse(' Codeforces ')}     ${chalk.dim('https://codeforces.com/submissions/kingjames')}
   `);
-}());
+})();
 
