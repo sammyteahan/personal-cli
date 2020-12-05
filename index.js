28c3d6943ebc9cbde53d3a53776d8a2bc3118351
sammyteahan <sammyteahan@gmail.com>
2020-12-05 13:20:13 -0700

    Fix layout

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const init = require('./src/init');
 
   console.log(`${chalk.italic('Software Developer at https://www.objectiveinc.com')}
 
-  ${chalk.hex('#6cc644').bold.inverse(' GitHub ')}         ${chalk.dim('https://github.com/sammyteahan')}
-  ${chalk.hex('#554488').bold.inverse(' GitLab ')}         ${chalk.dim('https://gitlab.com/sammyteahan')}
-  ${chalk.hex('#f48024').bold.inverse(' Stack Overflow ')} ${chalk.dim('https://stackoverflow.com/users/7003729/sammyteahan')}
-  ${chalk.hex('#C11D26').bold.inverse(' Codeforces ')}     ${chalk.dim('https://codeforces.com/submissions/kingjames')}
-  `);
+${chalk.hex('#6cc644').bold.inverse(' GitHub ')}         ${chalk.dim('https://github.com/sammyteahan')}
+${chalk.hex('#554488').bold.inverse(' GitLab ')}         ${chalk.dim('https://gitlab.com/sammyteahan')}
+${chalk.hex('#f48024').bold.inverse(' Stack Overflow ')} ${chalk.dim('https://stackoverflow.com/users/7003729/sammyteahan')}
+${chalk.hex('#C11D26').bold.inverse(' Codeforces ')}     ${chalk.dim('https://codeforces.com/submissions/kingjames')}
+`);
 })();
 
